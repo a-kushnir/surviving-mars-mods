cf38eb17219ffc7aa36925c04942e8226085e9ae
Andrew Kushnir <akushnir2009@gmail.com>
2022-06-12 18:12:11 -0600

    Tourist Specialization. Fixed outside visuals

--- Tourist Specialization/Code/Script.lua
+++ Tourist Specialization/Code/Script.lua
@@ -3,5 +3,12 @@ local base_MakeTourist = MakeTourist
 function MakeTourist(applicant)
 	applicant.traits[applicant.specialist] = nil
 	applicant.specialist = "Tourist"
-    base_MakeTourist(applicant)
+    return base_MakeTourist(applicant)
 end
+
+function OnMsg.ColonistAddTrait(colonist, trait_id, init)
+	if trait_id == "Tourist" then
+		colonist:SetSpecialization(trait_id, init)
+	end
+end
+OnMsg.ColonistAddTrait = ColonistAddTrait
